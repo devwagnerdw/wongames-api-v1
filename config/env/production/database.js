3c01a08b4729b9f0a45619951a059a4e8465d6dd
vagnealves <vagnerdw1@gmail.com>
2024-03-08 21:10:20 -0300

    postgres

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,25 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
         ssl: {
-          require: true,
           rejectUnauthorized: false,
         },
       },
-      options: {},
+      options: {
+        ssl: true,
+      },
     },
   },
 });
