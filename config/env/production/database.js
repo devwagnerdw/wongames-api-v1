8022decb96f5f46a676e01deac41ce886430d21a
vagnealves <vagnerdw1@gmail.com>
2024-03-28 17:01:33 -0300

    Configure PostgreSQL connection using pg-connection-string

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,28 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+const { parse } = require('pg-connection-string');
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({ env }) => {
+const config = env('DATABASE_URL') ? parse(env('DATABASE_URL')) : {};
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        options: {
+          ssl: true,
         },
       },
-      options: {
-        ssl: true,
-      },
     },
-  },
-});
+  };
+};
